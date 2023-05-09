52c539d8030edcdd3f77a317a3829208b1df6992
lucianosole <lucianosole@gmail.com>
2023-05-08 22:14:57 -0300

    after-class

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Cards, Footer, Header } from "../../components";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <h1>Dashboard</h1>
+      <Cards />
+      <Footer />
+    </>
+  );
+};
+
+export { Dashboard };
